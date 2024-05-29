439f6b080d679df1259b64e221de515b5638f895
tamaspenzes-2001 <penzestamas@proton.me>
2024-05-29 13:13:47 +0200

    setup project

---
+++ games.py
@@ -0,0 +1,15 @@
+import random
+from beaupy imprt confirm, prompt, select
+from beaupy.spinners import *
+from rich.console import Console
+
+money = 100
+
+#Write your game of chance functions here
+
+
+
+
+
+
+#Call your game of chance functions here
