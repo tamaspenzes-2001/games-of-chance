2d813a74bf630e16d67355e0b859d71b595e598a
tamaspenzes-2001 <penzestamas@proton.me>
2024-05-30 15:58:54 +0200

    implement cho han

--- main.py
+++ main.py
@@ -30,7 +30,27 @@ def coin_flip(bet_amount):
     return -(bet_amount)
 
 def cho_han(bet_amount):
-  pass
+  console.print("Your bet:")
+  bet = select(["Odd", "Even"], cursor="O" if os.name=="nt" else "💰")
+  clear_screen()
+  console.print(f"Your bet: {bet}")
+  console.print(f"Bet amount: {bet_amount}")
+  spinner_char = "■" if os.name=="nt" else "🎲"
+  spinner_animation = [f"{spinner_char}    \n      ", f"  {spinner_char}  \n      ", f"    {spinner_char}\n      ", f"      \n    {spinner_char}", f"      \n  {spinner_char}  ", f"      \n{spinner_char}    "]
+  spinner = Spinner(spinner_animation, "Roll dices...")
+  spinner.start()
+  sleep(5)
+  dice1 = random.randint(1, 6)
+  dice2 = random.randint(1, 6)
+  result = dice1 + dice2
+  spinner.stop()
+  console.print(f"The outcome is: {dice1} + {dice2} = {result}")
+  if (result % 2 == 0 and bet == "Even") or (result % 2 == 1 and bet == "Odd"):
+    console.print(f"[green1]You earned {bet_amount} dollars![/green1]")
+    return bet_amount
+  else:
+    console.print(f"[salmon1]You lost {bet_amount} dollars![/salmon1]")
+    return -(bet_amount)
 
 def card_draw(bet_amount):
   pass
